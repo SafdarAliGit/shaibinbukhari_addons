2998c57cd54d25adbac653cb54f253d54167e9ca
safdar211ali <safdar211@hotmail.com>
2024-10-23 18:41:33 +0500

    updated

---
+++ shaibinbukhari_addons/public/js/point-of-sale.bundle.js
@@ -0,0 +1,9 @@
+import "../../../../erpnext/erpnext/selling/page/point_of_sale/pos_item_selector.js";
+// import "../../../../erpnext/erpnext/selling/page/point_of_sale/pos_item_cart.js";
+import "shaibinbukhari_addons/public/js/pos_item_cart_override.js";
+import "../../../../erpnext/erpnext/selling/page/point_of_sale/pos_item_details.js";
+import "../../../../erpnext/erpnext/selling/page/point_of_sale/pos_number_pad.js";
+import "../../../../erpnext/erpnext/selling/page/point_of_sale/pos_payment.js";
+import "../../../../erpnext/erpnext/selling/page/point_of_sale/pos_past_order_list.js";
+import "../../../../erpnext/erpnext/selling/page/point_of_sale/pos_past_order_summary.js";
+import "../../../../erpnext/erpnext/selling/page/point_of_sale/pos_controller.js";
